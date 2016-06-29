312bb6ed59184f6f0318daf42f5dc201ed46da6d
Theo Markkus Paul <theo.markkus.paul@gmail.com>
2016-06-29 17:11:14 +1200

    Include fs module, add empty get route for games

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
-var express = require('express');
+var express = require('express'),
+    fs = require('fs');
 
 var app = express();
 
 app.get('/', function(req, res){
   res.send("Welcome to my app!");
-})
+});
+
+app.get('/games', function(req, res){
+
+});
 
 app.listen(3000, console.log("Server listening on localhost:3000 "))
