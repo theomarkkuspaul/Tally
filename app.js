867efe97ff9a16c020d86feaa85b3f9a62d486ec
Theo Markkus Paul <theo.markkus.paul@gmail.com>
2016-06-30 12:16:18 +1200

    GET route to all games returns all games from data file

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require('express'),
+    Q = require('q'),
     fs = require('fs');
 
 var app = express();
@@ -9,6 +10,24 @@ app.get('/', function(req, res){
 
 app.get('/games', function(req, res){
 
+  read('./data.json', function(err, result){
+    if (err) {
+      console.log("Something went wrong");
+    } else {
+      var games = JSON.parse(result).games;
+      res.send(games);
+    }
+  })
 });
 
 app.listen(3000, console.log("Server listening on localhost:3000 "))
+
+function read(filePath, callback){
+  fs.readFile(filePath, function(err, data){
+    if (err) {
+      return callback(err);
+    } else {
+      return callback(null, data);
+    }
+  });
+}
